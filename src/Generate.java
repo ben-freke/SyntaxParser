57a8ad57df9746f21e2b0a19b07d4eb3713d6d05
ben-freke <b.freke@lancaster.ac.uk>
2016-03-08 16:11:30 +0000

    Initial Commit

---
+++ src/Generate.java
@@ -0,0 +1,9 @@
+/**
+ * Created by benfreke on 07/03/2016.
+ */
+public class Generate extends AbstractGenerate {
+    @Override
+    public void reportError(Token token, String explanatoryMessage) throws CompilationException {
+
+    }
+}
---
+++ src/SyntaxAnalyser.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+/**
+ * Created by benfreke on 07/03/2016.
+ */
+public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
+
+    public SyntaxAnalyser(String filename){
+
+    }
+
+    @Override
+    void _statementPart_() throws IOException, CompilationException {
+
+    }
+
+    @Override
+    void acceptTerminal(int symbol) throws IOException, CompilationException {
+
+    }
+}
