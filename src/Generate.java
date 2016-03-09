c391a994bda223a6ca0c8c4cc8405ea2a18b2123
Ben Freke <b.freke@lancaster.ac.uk>
2016-03-09 22:01:10 +0000

    Submission

--- src/Generate.java
+++ src/Generate.java
@@ -6,34 +6,106 @@
  *
  */
 public class Generate extends AbstractGenerate {
+
+    /**
+     * Receives Token token and String explanatoryMessage to generate an error and output this to the user.
+     * It begins by spliting the explanatoryMessage to establish the filename.
+     * Then, this establishes what error it is actually dealing with. For example, if the SyntaxAnalyser reads
+     * in a blank token, when it is expecting an if or while, it may indicate that the programmer has not closed
+     * an if statement (e.g. while \<statement\> end).
+     * @param token
+     * @param explanatoryMessage
+     * @throws CompilationException
+     */
+
     @Override
     public void reportError(Token token, String explanatoryMessage) throws CompilationException {
         String exception = "";
-        if (explanatoryMessage == null)
+        String[] parts = explanatoryMessage.split(",");
+        explanatoryMessage = parts[0];
+        String filename = parts[1];
+        if (explanatoryMessage.equals(""))
         {
             switch (token.symbol) {
                 case Token.elseSymbol:
-                    exception = ("------- Error ------- \n Line: " + token.lineNumber + "\n Token: "+ token.text +" \n Message: Received: \"else\" without corresponding \"if\"");
+                    exception = (   "\r\n------- Error ------- \r\n " +
+                                    "File: " + filename + "\r\n " +
+                                    "Line: " + token.lineNumber + "\r\n " +
+                                    "Token: "+ token.text +" \r\n " +
+                                    "Message: Received: \"else\" without corresponding \"if\"\r\n");
                     break;
                 case Token.thenSymbol:
-                    exception = ("Error. Received \"then\" without corresponding \"if\"");
+                    exception = (   "\r\n------- Error ------- \r\n " +
+                            "File: " + filename + "\r\n " +
+                            "Line: " + token.lineNumber + "\r\n " +
+                            "Token: "+ token.text +" \r\n " +
+                            "Message: Received: \"then\" without corresponding \"if\"\r\n");
                     break;
                 case Token.loopSymbol:
-                    exception = ("Error. Received \"loop\" without corresponding \"while\"");
+                    exception = (   "\r\n------- Error ------- \r\n " +
+                            "File: " + filename + "\r\n " +
+                            "Line: " + token.lineNumber + "\r\n " +
+                            "Token: "+ token.text +" \r\n " +
+                            "Message: Received: \"loop\" without corresponding \"while\"\r\n");
                     break;
                 case Token.untilSymbol:
-                    exception = ("Error. Received \"until\" without corresponding \"do\"");
+                    exception = (   "\r\n------- Error ------- \r\n " +
+                            "File: " + filename + "\r\n " +
+                            "Line: " + token.lineNumber + "\r\n " +
+                            "Token: "+ token.text +" \r\n " +
+                            "Message: Received: \"until\" without corresponding \"do\"\r\n");
                     break;
             }
         }
-        else if (explanatoryMessage.equals("EOF"))exception = ("------- Error ------- \n Line: " + token.lineNumber + "\n Token: "+ token.text +"\n Expecting: " + explanatoryMessage + " \n Message: Received: \"" + token.text + "\" after calling \"end\"\n");
-        else if (token.symbol == Token.endSymbol) exception = ("Error. Received: \"" + token.text + "\" on Line " + token.lineNumber + " after receiving \";\".");
-        else if (token.text != "") exception = ("Error. Received: \"" + token.text + "\" on Line " + token.lineNumber + ". Expecting: \"" + explanatoryMessage + "\"");
-        else exception = ("Error. Did not receive closing statement for \"" + explanatoryMessage + "\" on Line " + token.lineNumber);
+        else if (explanatoryMessage.equals("EOF"))
+            exception =
+                        (
+                                "\r\n------- Error ------- \r\n " +
+                                "File: " + filename + "\r\n " +
+                                "Line: " + token.lineNumber + "\r\n " +
+                                "Token: " + token.text +"\r\n " +
+                                "Expecting: " + explanatoryMessage + " " +
+                                "\r\n Message: Received \"" + token.text + "\" after calling \"end\"\r\n"
+                        );
+
+        else if (token.symbol == Token.endSymbol)
+            exception =
+                (
+                        "\r\n------- Error ------- \r\n " +
+                                "File: " + filename + "\r\n " +
+                                "Line: " + token.lineNumber + "\r\n " +
+                                "Token: " + token.text +"\r\n " +
+                                "Expecting: " + explanatoryMessage + " " +
+                                "\r\n Message: Received \"" + token.text + "\" after calling \";\"\r\n"
+                );
+
+        else if (token.text != "")
+
+            exception =
+                    (
+                            "\r\n------- Error ------- \r\n " +
+                                    "File: " + filename + "\r\n " +
+                                    "Line: " + token.lineNumber + "\r\n " +
+                                    "Token: " + token.text +"\r\n " +
+                                    "Expecting: " + explanatoryMessage + " " +
+                                    "\r\n Message: Received \"" + token.text + "\"\r\n"
+                    );
+
+        else
+
+            exception =
+                    (
+                            "\r\n------- Error ------- \r\n " +
+                                    "File: " + filename + "\r\n " +
+                                    "Line: " + token.lineNumber + "\r\n " +
+                                    "Token: " + token.text +"\r\n " +
+                                    "Expecting: " + explanatoryMessage + " " +
+                                    "\r\n Message: Did not receive closing statement for " + explanatoryMessage + "\r\n"
+                    );
+
         System.out.println(exception);
         throw new CompilationException(exception);
 
     }
-
 
 }
--- src/SyntaxAnalyser.java
+++ src/SyntaxAnalyser.java
@@ -9,7 +9,14 @@
 import java.io.IOException;
 
 public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
+    private String file;
+
+    /**
+     * Super constructor which creates the Lexical Analyser from the filename.
+     * @param filename
+     */
     public SyntaxAnalyser(String filename){
+        file = filename;
         try {
             lex = new LexicalAnalyser(filename);
         } catch (Exception e)
@@ -18,8 +25,14 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         }
     }
 
+    /**
+     * Inherited from abstract class.
+     * Begins the work on the top level token.
+     * @throws IOException
+     * @throws CompilationException
+     */
     @Override
-    void _statementPart_() throws IOException, CompilationException {
+    public void _statementPart_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<statement part>");
         acceptTerminal(Token.beginSymbol);
         _statementList_();
@@ -27,12 +40,20 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _statementList_() throws IOException, CompilationException {
+    /**
+     * Loops through each statement encapsulated with a 'begin' and 'end'
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _statementList_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<statement list>");
         while (nextToken.symbol != Token.endSymbol)
         {
             _statement_();
-            if (nextToken.symbol != Token.endSymbol) acceptTerminal(Token.semicolonSymbol);
+            if (nextToken.symbol != Token.endSymbol){
+                acceptTerminal(Token.semicolonSymbol);
+                if (nextToken.symbol == Token.endSymbol) acceptTerminal(0);
+            }
             else {
                 break;
             }
@@ -41,7 +62,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         acceptTerminal(Token.endSymbol);
     }
 
-    void _statement_() throws IOException, CompilationException {
+    /**
+     * Takes the first token of a statement and determines which type of statement it is, for example a procedure.
+     * Once the type has been determined, the method runs the relevant method representing a non-terminal.
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _statement_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<statement>");
         switch (nextToken.symbol)
         {
@@ -65,7 +92,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _assignmentStatement_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal assignment statement. Assignment statement must
+     * be a identifier becoming an expression, or an identifier becoming a stringConstant
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _assignmentStatement_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<assignment statement>");
         acceptTerminal(Token.identifier);
         acceptTerminal(Token.becomesSymbol);
@@ -80,7 +113,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         myGenerate.finishNonterminal("<assignment statement>");
     }
 
-    void _procedureStatement_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal procedure statement. Procedure statement must have terminal 'call' followed
+     * by an identifier and then the non-terminal argument list
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _procedureStatement_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<procedure statement>");
         acceptTerminal(Token.callSymbol);
         acceptTerminal(Token.identifier);
@@ -91,7 +130,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _argumentList_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal argument list. This can either be an identifier, or an argument list followed
+     * by an identifier.
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _argumentList_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<argument list>");
         switch (nextToken.symbol)
         {
@@ -107,7 +152,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _untilStatement_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal until statement. This must receive terminal do, followed by a statement list,
+     * followed by terminal until, followed by a condition.
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _untilStatement_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<until statement>");
         acceptTerminal(Token.doSymbol);
         _statementList_();
@@ -117,7 +168,14 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _whileStatement_() throws IOException, CompilationException {
+
+    /**
+     * The method for the non-terminal until statement. This statement must receive terminal while, followed by a
+     * condition, followed by terminal loop, followed by a statement list, followed by terminals end and loop.
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _whileStatement_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<while statement>");
         acceptTerminal(Token.whileSymbol);
         _condition_();
@@ -128,7 +186,14 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _ifStatement_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal if statement. This statement must receive terminal if, followed by non-terminal
+     * condition, followed by terminal then, followed by non-terminal statement list and finally followed by terminals
+     * end and if
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _ifStatement_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<if statement>");
         acceptTerminal(Token.ifSymbol);
         _condition_();
@@ -153,7 +218,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         myGenerate.finishNonterminal("<if statement>");
     }
 
-    void _expression_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal expression. This can either be the non-terminal term, or terminal + followed by
+     * non-terminal term, or terminal - followed by non-terminal term
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _expression_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<expression>");
         _term_();
         while (nextToken.symbol == Token.plusSymbol || nextToken.symbol == Token.minusSymbol)
@@ -165,7 +236,12 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         myGenerate.finishNonterminal("<expression>");
     }
 
-    void _factor_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal factor. This can either by an identifier, or a number constant, or an expression
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _factor_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<factor>");
         switch (nextToken.symbol)
         {
@@ -187,7 +263,13 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
 
     }
 
-    void _term_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal term. This can either be a factor, or a terminal * followed by a factor, or a
+     * terminal / followed by a factor
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _term_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<term>");
         _factor_();
         while (nextToken.symbol == Token.timesSymbol || nextToken.symbol == Token.divideSymbol)
@@ -199,60 +281,81 @@ public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
         myGenerate.finishNonterminal("<term>");
     }
 
-    void _condition_() throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal condition. This can either be the terminal identifier, followed by a
+     * conditional operator, followed by another identifier, or be an identifier followed by a conditional operator
+     * followed by a number constant, or the same but followed by a string constant
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _condition_() throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<condition>");
         acceptTerminal(16);
         _conditionalOperator_();
         switch (nextToken.symbol){
-            case 16:
-                acceptTerminal(16);
+            case Token.identifier:
+                acceptTerminal(Token.identifier);
                 break;
-            case 26:
-                acceptTerminal(26);
+            case Token.numberConstant:
+                acceptTerminal(Token.numberConstant);
                 break;
-            case 31:
-                acceptTerminal(31);
+            case Token.stringConstant:
+                acceptTerminal(Token.stringConstant);
                 break;
         }
         myGenerate.finishNonterminal("<condition>");
 
     }
 
-    public void _conditionalOperator_()  throws IOException, CompilationException {
+    /**
+     * The method for the non-terminal conditional operator, which ensures the token meets the possible conditional
+     * operators available
+     * @throws IOException
+     * @throws CompilationException
+     */
+    private void _conditionalOperator_()  throws IOException, CompilationException {
         myGenerate.commenceNonterminal("<conditional operator>");
         switch (nextToken.symbol){
-            case 14:
-                acceptTerminal(14);
+            case Token.greaterEqualSymbol:
+                acceptTerminal(Token.greaterEqualSymbol);
                 break;
-            case 15:
-                acceptTerminal(15);
+            case Token.greaterThanSymbol:
+                acceptTerminal(Token.greaterThanSymbol);
                 break;
-            case 11:
-                acceptTerminal(11);
+            case Token.equalSymbol:
+                acceptTerminal(Token.equalSymbol);
                 break;
-            case 25:
-                acceptTerminal(25);
+            case Token.notEqualSymbol:
+                acceptTerminal(Token.notEqualSymbol);
                 break;
-            case 21:
-                acceptTerminal(21);
+            case Token.lessEqualSymbol:
+                acceptTerminal(Token.lessEqualSymbol);
                 break;
-            case 22:
-                acceptTerminal(22);
+            case Token.lessThanSymbol:
+                acceptTerminal(Token.lessThanSymbol);
                 break;
         }
         myGenerate.finishNonterminal("<conditional operator>");
 
     }
 
+    /**
+     * Receives integer symbol (the integer representation of the received token). From this, checks whether the
+     * symbol is expected. If it is, it runs myGenerate.insertTerminal(nextToken) and moves the lexical analyser
+     * forwards, otherwise it generates an error, reporting this with the myGenerate class and throwing a
+     * CompilationException.
+     * @param symbol
+     * @throws IOException
+     * @throws CompilationException
+     */
     @Override
     void acceptTerminal(int symbol) throws IOException, CompilationException {
         if (nextToken.symbol != symbol) {
             if (symbol == Token.semicolonSymbol)
             {
-                myGenerate.reportError(nextToken, null);
+                myGenerate.reportError(nextToken, "," + file);
             }
-            myGenerate.reportError(nextToken, Token.getName(symbol));
-            throw new CompilationException();
+            myGenerate.reportError(nextToken, (symbol == 0 ? "Statement" : Token.getName(symbol)) + "," + file);
         } else {
             myGenerate.insertTerminal(nextToken);
             nextToken = lex.getNextToken();
